77ab095414fa3610012ae563884574cd6a147b8a
Nikita Vanda <nikitavanda149@proton.me>
2024-12-07 17:13:32 +0200

    add mission and target model

--- agency_tasks_app/models.py
+++ agency_tasks_app/models.py
@@ -3,11 +3,36 @@ from django.db import models
 from .validators import positive_validator, breed_validator
 
 
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    is_complete = models.BooleanField()
+
+    class Meta:
+        abstract = True
+
+
 class SpyCat(models.Model):
     name = models.CharField(max_length=50)
     work_experience = models.PositiveSmallIntegerField()
     breed = models.CharField(max_length=100, validators=[breed_validator])
     salary = models.DecimalField(max_digits=8, decimal_places=2, validators=[positive_validator])
+
+    def __str__(self):
+        return self.name
+
+
+class Mission(Task):
+    spy_cat = models.ForeignKey(SpyCat, on_delete=models.SET_NULL, null=True, related_name='mission')
+
+    def __str__(self):
+        return self.name
+
+
+class Target(Task):
+    country = models.CharField(max_length=100)
+    notes = models.TextField()
+
+    mission = models.ForeignKey(Mission, on_delete=models.SET_NULL, null=True, related_name='targets')
 
     def __str__(self):
         return self.name
